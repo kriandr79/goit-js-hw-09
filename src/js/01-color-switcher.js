93024aee32822067bb7b47ea4bc9ac374388b589
kriandr79 <127450901+kriandr79@users.noreply.github.com>
2023-08-05 01:17:29 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
 
+const ACTION_INTERVAL = 1000;
+let isStopped = true;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    isStopped = false;
+    refs.startBtn.setAttribute('disabled', 'disabled'); // деактивуємо кнопку Старт
+
+  const timerId = setInterval(() => {
+    if (isStopped) {
+      clearInterval(timerId);
+      refs.startBtn.removeAttribute('disabled'); // активуємо кнопку Старт
+      return;
+    }
+    
+    refs.bodyEl.style.backgroundColor = getRandomHexColor();
+  }, ACTION_INTERVAL);
+}
+
+function onStopBtnClick() {
+    if (isStopped) {
+        return;
+    }
+    isStopped = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
