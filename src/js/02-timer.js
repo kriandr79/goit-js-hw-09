0fa1dfc22a7815052d366f8d4d84a2d910a0b928
kriandr79 <127450901+kriandr79@users.noreply.github.com>
2023-08-05 16:20:22 +0300

    task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,85 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  input: document.getElementById('datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+
+refs.startBtn.setAttribute('disabled', 'disabled'); // деактивуємо кнопку Start
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = new Date(selectedDates[0]);
+    const currentDate = Date.now(); // поточний час
+
+    if (selectedDate.getTime() < currentDate) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      Notiflix.Notify.success('You choose a right date');
+      refs.startBtn.removeAttribute('disabled'); // активуємо кнопку Start
+    }
+  },
+};
+
+flatpickr(refs.input, options);
+
+function onStartBtnClick() {
+  refs.startBtn.setAttribute('disabled', 'disabled'); // деактивуємо кнопку Start
+
+  const selectedTime = new Date(
+    refs.input._flatpickr.selectedDates[0]
+  ).getTime(); // беремо час який вибрано в інпуті
+
+  const intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = selectedTime - currentTime;
+
+    // Перевірка коли таймер дійде до 0
+    if (deltaTime <= 0) {
+      clearInterval(intervalId);
+      Notiflix.Notify.info('The time is gone');
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    refs.days.textContent = addLeadingZero(days);
+    refs.hours.textContent = addLeadingZero(hours);
+    refs.minutes.textContent = addLeadingZero(minutes);
+    refs.seconds.textContent = addLeadingZero(seconds);
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
